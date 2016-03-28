6076a9185f5e31140f238c132da2c0401cd65365
I Have No Name <jonathaneldy.08@gmail.com>
2016-03-28 21:59:26 +0800

    Update Object3D.java

--- src/main/java/iH4v3n0N4m3/game/e3d/Object3D.java
+++ src/main/java/iH4v3n0N4m3/game/e3d/Object3D.java
@@ -1,5 +1,7 @@
 package iH4v3n0N4m3.game.e3d;
 
-public abstract class Object3D{
- public abstract void render3D(Vertex3D v);
+import iH4v3n0N4m3.ui.swing.Panel;
+
+public abstract class Object3D extends Panel{
+ public abstract void render3D(Vertex3D v,Camera c);
 }
