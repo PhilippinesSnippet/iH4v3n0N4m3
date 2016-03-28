a3e6e4172073942ca8b621cf860a55749de75c31
I Have No Name <jonathaneldy.08@gmail.com>
2016-03-28 17:02:15 +0800

    Create Camera.java

---
+++ src/main/java/iH4v3n0N4m3/game/e3d/Camera.java
@@ -0,0 +1,11 @@
+package iH4v3n0N4m3.game.e3d;
+
+public interface Camera{
+ public void rotate(int a);
+ public void moveForward(int a);
+ public void moveBackward(int a);
+ public void moveLeftward(int a);
+ public void moveRightward(int a);
+ 
+ protected Camera();
+}
