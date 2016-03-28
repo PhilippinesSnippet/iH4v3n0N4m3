980e73b984c9ed7e777527e5d1d11dbfd1407946
I Have No Name <jonathaneldy.08@gmail.com>
2016-03-28 22:16:17 +0800

    Create IPlugin.java

---
+++ src/main/java/iH4v3n0N4m3/plugins/IPlugin.java
@@ -0,0 +1,8 @@
+package iH4v3n0N4m3.plugins;
+
+import iH4v3n0N4m3.ui.swing.InternalFrame;
+
+public interface IPlugin{
+ public void onLoad(InternalFrame i);
+ public void onUnload(InternalFrame i);
+}
